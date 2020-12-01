6b367dfd394a8a37ddbbe233a6b36f65d56f529b
Bartosz Piekarski <21hiddenshadow@gmail.com>
2020-12-01 19:13:03 +0100

    Zmiany w strukturze 2

--- build.sh
+++
@@ -1,3 +0,0 @@
-echo "Starting build..."
-"C:\Program Files\Unity\Hub\Editor\2019.4.13f1\Editor\Unity.exe" -quit -batchmode -projectPath "C:\Users\kalcz\Documents\UnityProjects\MultiTest" -executeMethod BuildScript.BuildAll
-echo "Finished build!"
