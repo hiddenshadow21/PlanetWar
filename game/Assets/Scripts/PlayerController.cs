77b9eb144e07d397c01436bc9af31f4e8290020f
Bartosz Piekarski <21hiddenshadow@gmail.com>
2020-12-14 14:09:18 +0100

    Skok

--- game/Assets/Scripts/PlayerController.cs
+++ game/Assets/Scripts/PlayerController.cs
@@ -5,18 +5,16 @@ using UnityEngine;
 
 public class PlayerController : NetworkBehaviour
 {
-    private float moveSpeed = 50f;
+    private float moveSpeed = 10f;
+    private float jumpHeight = 3f;
     private Vector2 moveDir;
+
+    public new Collider2D collider;
     public Rigidbody2D rb;
 
-    void HandleMovement()
-    {
-        if (!isLocalPlayer)
-            return;
-        float moveHorizontal = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
-        float moveVertical = 0; //Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
-        moveDir = new Vector2(moveHorizontal, moveVertical);
-
+    public  bool isGrounded { 
+        get;
+        private set;
     }
 
     void Update()
@@ -24,8 +22,44 @@ public class PlayerController : NetworkBehaviour
         HandleMovement();
     }
 
-    void FixedUpdate()
+    private void FixedUpdate()
     {
-        rb.MovePosition(rb.position + (Vector2)transform.TransformDirection(moveDir) * moveSpeed * Time.deltaTime);
+        CheckIfOnGround();
     }
+
+    void HandleMovement()
+    {
+        if (!isLocalPlayer)
+            return;
+        float moveHorizontal = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        moveDir = new Vector2(moveHorizontal, 0);
+        transform.Translate(moveDir);
+
+        if (isGrounded && Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(transform.up * 1000 * jumpHeight * Time.deltaTime);
+        }
+    }
+
+    void CheckIfOnGround()
+    {
+        if (!isLocalPlayer)
+            return;
+        isGrounded = false;
+        var t = transform.TransformDirection(
+            transform.InverseTransformDirection(transform.position)
+            - new Vector3(0, collider.bounds.extents.y, 0)
+        );
+        RaycastHit2D hit = Physics2D.Raycast(
+            t,
+            -transform.up);
+        if (hit.collider != null)
+        {
+
+            if (hit.distance <= 0.2f)
+                isGrounded = true;
+        }
+            
+    }
+    
 }
