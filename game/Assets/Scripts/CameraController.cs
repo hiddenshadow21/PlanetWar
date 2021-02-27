033396c72d74d95ea78cd019a48cb91e68cf28d4
Bartosz Piekarski <21hiddenshadow@gmail.com>
2021-02-27 18:16:59 +0100

    Kamera podąża za graczem

---
+++ game/Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+
+    void Update()
+    {
+        LockCameraToPlayer();
+    }
+
+    private void LockCameraToPlayer()
+    {
+        Vector3 pos = player.transform.position;
+        pos.z = -10;
+        transform.position = pos;
+    }
+}
