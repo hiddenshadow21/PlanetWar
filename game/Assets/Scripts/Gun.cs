113074d3dd49685369fe3d652d075fdd3a298415
Bartosz Piekarski <21hiddenshadow@gmail.com>
2021-03-03 17:37:31 +0100

    Celowanie bronią

---
+++ game/Assets/Scripts/Gun.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float fireRate = 40f;
+    public float reloadSpeed = 1f;
+    public int maxAmmo = 15;
+
+    public GameObject weaponBullet;
+    public Transform weaponFirePosition;
+}
---
+++ game/Assets/Scripts/PlayerAimWeapon.cs
@@ -0,0 +1,28 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAimWeapon : NetworkBehaviour
+{
+    private Transform aimTransform;
+
+    private void Awake()
+    {
+        aimTransform = transform.Find("Aim");
+    }
+
+    private void Update()
+    {
+        if (!isLocalPlayer)
+            return;
+
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0f;
+
+        Vector3 aimDirection = (mousePos - transform.position).normalized;
+        float angle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
+        aimTransform.eulerAngles = new Vector3(0, 0, angle);
+    }
+}
+
