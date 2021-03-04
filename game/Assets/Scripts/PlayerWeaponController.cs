0ce4ded46cb85310f87d583f04444862ff23f4a7
Bartosz Piekarski <21hiddenshadow@gmail.com>
2021-03-05 00:27:35 +0100

    Zmiana broni

--- game/Assets/Scripts/PlayerWeaponController.cs
+++ game/Assets/Scripts/PlayerWeaponController.cs
@@ -1,4 +1,5 @@
 ﻿using Mirror;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,10 +7,46 @@ using UnityEngine;
 public class PlayerWeaponController : NetworkBehaviour
 {
     private Transform aimTransform;
+    private int selectedWeaponLocal = 1;
+    public Gun[] weapons;
+
+    [SyncVar(hook = nameof(OnWeaponChanged))]
+    public int activeWeaponSynced = 1;
+
+    void OnWeaponChanged(int _Old, int _New)
+    {
+        // disable old weapon
+        // in range and not null
+        if (_Old < weapons.Length && weapons[_Old] != null)
+        {
+            weapons[_Old].gameObject.SetActive(false);
+        }
+
+        // enable new weapon
+        // in range and not null
+        if (_New < weapons.Length && weapons[_New] != null)
+        {
+            weapons[_New].gameObject.SetActive(true);
+        }
+    }
+
+    [Command]
+    public void CmdChangeActiveWeapon(int newIndex)
+    {
+        activeWeaponSynced = newIndex;
+    }
+
 
     private void Awake()
     {
         aimTransform = transform.Find("Aim");
+        foreach (var item in weapons)
+        {
+            if (item != null)
+            {
+                item.gameObject.SetActive(false);
+            }
+        }
     }
 
     private void Update()
@@ -17,6 +54,31 @@ public class PlayerWeaponController : NetworkBehaviour
         if (!isLocalPlayer)
             return;
         HandleAiming();
+        HandleShooting();
+        HandleWeaponSwitching();
+    }
+
+    private void HandleWeaponSwitching()
+    {
+        if (Input.GetButtonDown("Fire2")) //Fire2 is mouse 2nd click and left alt
+        {
+            selectedWeaponLocal += 1;
+
+            if (selectedWeaponLocal >= weapons.Length)
+            {
+                selectedWeaponLocal = 0;
+            }
+
+            CmdChangeActiveWeapon(selectedWeaponLocal);
+        }
+    }
+
+    private void HandleShooting()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            //TODO: Shooting
+        }
     }
 
     private void HandleAiming()
