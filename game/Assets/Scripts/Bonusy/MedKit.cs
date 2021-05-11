124209e89eab2b5000d3011730707b5d47fb9351
Bartosz Piekarski <21hiddenshadow@gmail.com>
2021-05-11 14:16:04 +0200

    Abstrakcyjna klasa Bonus i MedKit

---
+++ game/Assets/Scripts/Bonusy/MedKit.cs
@@ -0,0 +1,32 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MedKit : BonusBase
+{
+
+    [SerializeField]
+    private float RestoreAmount = 10;
+
+    [Server]
+    protected override void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (!isServer)
+            return;
+
+        Debug.Log(collider.gameObject.name);
+
+        var player = collider.gameObject.GetComponent<PlayerController>();
+        if (player == null)
+            return;
+
+        player.AddHealth(RestoreAmount);
+        NetworkServer.Destroy(gameObject);
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+    }
+}
