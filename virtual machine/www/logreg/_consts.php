b279bb389021d29d74dfd1aafcedfc62ffb1fa07
Krzysztof Kruglak <kru_krz@o2.pl>
2021-06-28 08:43:27 +0200

    LogReg 9.1
    Przejście na nową bazę danych.

--- virtual machine/www/logreg/_consts.php
+++ virtual machine/www/logreg/_consts.php
@@ -1,9 +1,9 @@
 <?php
 	
 $SERVERNAME="wwwlab.uci.umk.pl";
-$DBNAME="296766_1m9";
-$LOGIN="296766_1m9ad";
-$PASSWORD="hmemyBtftZpY";
+$DBNAME="296766_ks3";
+$LOGIN="296766_ks3ad";
+$PASSWORD="HkKthB1_ejYn";
 $RESTRICTED_MARK=":";
 $ADUA_TIME=172800; //in seconds
 $TIME_OFFSET=7200; //in seconds
